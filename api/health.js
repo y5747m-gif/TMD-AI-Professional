2e5d83f0a2cc96a9a0c0a6a08ffe59ec2515691d
y5747m-gif <y5747m@gmail.com>
2026-09-03 16:46:10 +0300

    Refactor Cache-Control header setting

--- api/health.js
+++ api/health.js
@@ -1,5 +1,8 @@
 module.exports = async function handler(req, res) {
-  res.setHeader("Cache-Control", "no-store");
+  res.setHeader(
+    "Cache-Control",
+    "no-store"
+  );
 
   return res.status(200).json({
     ok: true,
